1a1f2340d5874ef4e55ecf97795fc9155178bb42
mahendharbillakuri <billakurimahendhar@gmail.com>
2025-08-10 23:11:19 +0530

    Added subscription routes with placeholders

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -7,3 +7,16 @@ export const createSubscription = async (req, res) => {
     next(e);
   }
 };
+export const getUserSubscriptions = async (req, res,next) => {
+  try {
+    if(req.user.id!== req.params.id) {
+    const error=new Error('You are not authorized to view this user\'s subscriptions');
+    error.statusCode = 401;
+    throw error;
+    }
+    const subscriptions = await Subscription.find({ user: req.params.id });
+    res.status(200).json({ success: true, data: subscriptions });
+  } catch (e) {
+    next(e);
+  }
+};
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -2,23 +2,31 @@ import express from 'express';
 import { Router } from "express";
 import authorize from '../middlewares/auth.middleware.js';
 import { createSubscription } from '../controllers/subscription.controller.js';
+import { getUserSubscriptions } from '../controllers/subscription.controller.js';
 const subscriptionRouter = Router();
+
+
 subscriptionRouter.get('/', (req, res) => {
     res.send({ title: 'get all subcriptions' });
 });
+
+
 subscriptionRouter.get('/:id', (req, res) => {
     res.send({ title: 'get subcription' });
 });
+
+
 subscriptionRouter.post('/', authorize,createSubscription);
+
 subscriptionRouter.put('/:id', (req, res) => {
     res.send({ title: 'update subcriptions' });
 });
 subscriptionRouter.delete('/:id', (req, res) => {
     res.send({ title: 'delete subcriptions' });
 });
-subscriptionRouter.get('/user/:id', (req, res) => {
-    res.send({ title: 'get all  subcriptions of user' });
-});
+
+subscriptionRouter.get('/user/:id', authorize, getUserSubscriptions);
+
 subscriptionRouter.put('/:id/cancel', (req, res) => {
     res.send({ title: 'cancel subscription' });
 });
